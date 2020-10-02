6ae2a938187fd54ba21c462cf51773dc1724f0f5
Jang Hyejeong <hj970219@naver.com>
2020-10-03 00:16:25 +0900

    Delete 완주하지 못한 선수.py

--- Programmers/완주하지 못한 선수.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    answer = ''
-    for i in completion:
-        for j in range(len(participant)):
-            if i==participant[j]:
-                participant.pop(j)
-                break
-    answer=participant[0]
-    return answer
