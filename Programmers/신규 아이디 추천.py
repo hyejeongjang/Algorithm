21d4c05999a0f30f08c89e8f8e6bf8d12eee6f54
Jang Hyejeong <hj970219@naver.com>
2022-05-27 00:38:54 +0900

    Create 신규 아이디 추천.py
    22.05.26 스터디

---
+++ Programmers/신규 아이디 추천.py
@@ -0,0 +1,16 @@
+def solution(lottos, win_nums):
+    answer = []
+    lottos.sort()
+    win_nums.sort()
+    if(lottos==win_nums):
+        answer=[1,1]
+    elif lottos.count(0)==6:
+        answer=[1,6]
+    elif len(set(lottos) & set(win_nums))==0 and lottos.count(0)==0: 
+        answer=[6,6]
+    else:
+        both=len(set(lottos) & set(win_nums)) # 교집합
+        count_zero=int(lottos.count(0))
+        answer.append(7-both-count_zero) # 
+        answer.append(7-both)
+    return answer
