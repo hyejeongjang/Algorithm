08c605438a56b92ef9e66dc250b47a56d3e6eb4d
Jang Hyejeong <hj970219@naver.com>
2022-07-20 21:05:21 +0900

    0721 스터디

---
+++ Baekjoon/2812.py
@@ -0,0 +1,18 @@
+# 2812 크게 만들기
+
+import itertools
+
+n,k=map(int, input().split())
+num=list(input())
+stack=[]
+
+for number in num:
+    while stack and stack[-1]<number and k>0:
+        stack.pop()
+        k-=1
+    stack.append(number)
+
+if k>0:
+    print(''.join(stack[:k]))
+else:
+    print(''.join(stack))
