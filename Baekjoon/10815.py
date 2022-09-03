85edcb4e3ca7ce20cd299face11f61a0b2610aac
Jang Hyejeong <hj970219@naver.com>
2022-09-03 11:50:43 +0900

    Update 10815.py

--- Baekjoon/10815.py
+++ Baekjoon/10815.py
@@ -3,13 +3,25 @@ import sys
 input=sys.stdin.readline
 
 n=int(input())
-num_n=set(map(int, input().split()))
+num_n=list(map(int, input().split()))
+num_n.sort()
 
 m=int(input())
 num_m=list(map(int, input().split()))
 
+# 이진 탐색
 for i in num_m:
-    if i in num_n:
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+    start, end=0, n-1
+
+    while True:
+        mid=(start+end)//2
+        if start>end:
+            print(0, end=' ')
+            break
+        elif num_n[mid]>i:
+            end=mid-1
+        elif num_n[mid]<i:
+            start=mid+1
+        elif num_n[mid]==i:
+            print(1, end=' ')
+            break
